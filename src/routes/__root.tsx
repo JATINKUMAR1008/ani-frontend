139c7f7032f47498846342c06fa3f956a0ad66de
Jatin Kumar <jatinkumar10082003@gmail.com>
2024-12-03 19:38:43 +0530

    removing devtools from prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { AppProvider } from "@/components/providers/app-provider";
 export const Route = createRootRoute({
   component: RootComponent,
@@ -13,7 +12,7 @@ function RootComponent() {
       {/* <div>Hello "__root"!</div> */}
       <Outlet />
       </AppProvider>
-      <TanStackRouterDevtools />
+      {/* <TanStackRouterDevtools /> */}
     </React.Fragment>
   );
 }
